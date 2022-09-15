d782ab91a46f0095a0cb50da48a4d0997fee33ae
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-09-15 13:17:01 +0300

    python_basic_data_types

--- Ch2 Data Types, Values, Variables, and Names/Puthon_types.py
+++ Ch2 Data Types, Values, Variables, and Names/Puthon_types.py
@@ -1,18 +1,29 @@
 # Имя                       Тип       Изменяемый? Примеры
 # Булево.значение           bool       Нет        True, False
-# Целое.число               int        Нет        47,.25000,.25_000
-# Число.с.плавающей точкой  float      Нет        3.14,.2.7e5
-# Комплексное число         complex    Нет        3j,.5.+.9j
-# Текстовая строка          str        Нет        'alas',."alack",.'''a.verse.attack'''
-# Кортеж                    tuple      Нет         (2,.4,.8)
+# Целое.число               int        Нет        47, 25000, 25_000
+# Число.с.плавающей точкой  float      Нет        3.14, 2.7e5
+# Комплексное число         complex    Нет        3j,5 + 9j
+# Текстовая строка          str        Нет        'alas',"alack",'''a verse attack'''
+# Кортеж                    tuple      Нет         (2,4, 8)
 # Байты                     bytes      Нет         b'ab\xff'
-# Фиксированное множество   frozenset  Нет         frozenset(['Elsa',.'Otto'])
+# Фиксированное множество   frozenset  Нет         frozenset(['Elsa','Otto'])
 # Словарь                   dict       Да          {'game':'bingo','dog':'dingo','drummer':'Ringo'}
-# Список                    list       Да          ['Winken',.'Blinken',.'Nod']
+# Список                    list       Да          ['Winken','Blinken','Nod']
 # Массив байтов             bytearray  Да          bytearray(…)
-# Множество                 set        Да          set([3,.5,.7])
+# Множество                 set        Да          set([3, 5, 7])
+
+s = ""
 
 
+def replace_sep_in_sting(string):
+    print(' '.join(string.split('.')))
 
 
+replace_sep_in_sting(s)
 
+"""Python является строго типизированным языком, а это означает, что тип объекта не изменяется,
+даже если его значение изменяемо"""
+a = 5
+print(f'a = {a}', type(a))
+b = str(a)
+print(f'b = {b}', type(b))
