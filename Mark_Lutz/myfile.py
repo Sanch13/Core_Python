e8710f558878acb819bb19b79a0814db1c0c594f
a.zubchyk <Sanch13sanch@gmail.com>
2022-11-04 17:00:37 +0300

    mark_lutz

--- Mark_Lutz/myfile.py
+++ Mark_Lutz/myfile.py
@@ -1,5 +1,23 @@
-title = "The meaning of life"
-a = "dead"
-b = "parrot"
-c = "sketch"
-print(a, b, c)
+from timeit import timeit
+
+# title = "The meaning of life"
+# a = "dead"
+# b = "parrot"
+# c = "sketch"
+# print(a, b, c)
+
+
+def list_comp():
+    squares = [x ** 2 for x in range(1000)]
+    return squares
+
+
+def ffor():
+    squares = []
+    for x in range(1000):
+        squares.append(x**2)
+    return squares
+
+
+print(timeit("list_comp()", globals=globals(), number=10000))
+print(timeit("ffor()", globals=globals(), number=10000))
--- Mark_Lutz/script1.py
+++ Mark_Lutz/script1.py
@@ -1,6 +1,16 @@
 import sys
 
 print(sys.version)
-print(2**32)
-x = "Spam!"
-print(x*8)
+# print(2**32)
+# x = "Spam!"
+# print(x*8)
+
+s ='spam'
+raw = open('data.txt', 'rb').read()
+print(raw, len(raw))
+
+d1 = {'a': 1, 'b': 2, 'c': 3,}
+d2 = {'a': 1, 'b': 2, 'c': 3, 'd': 5}
+b = 4
+a = 3
+print(b / (2.0 + a))
