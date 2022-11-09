a26d701fd1e61393921162333e04bfc04611bf75
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-11-09 17:30:00 +0300

    Be_a_Pythonista

--- Mark_Lutz/script1.py
+++ Mark_Lutz/script1.py
@@ -1,28 +1,6 @@
 import sys
 
 print(sys.version)
-# print(2**32)
-# x = "Spam!"
-# print(x*8)
-
-# s ='spam'
-# raw = open('data.txt', 'rb').read()
-# print(raw, len(raw))
-#
-# d1 = {'a': 1, 'b': 2, 'c': 3,}
-# d2 = {'a': 1, 'b': 2, 'c': 3, 'd': 5}
-# b = 4
-# a = 3
-# print(b / (2.0 + a))
-
-
-# from collections import namedtuple
-#
-#
-# person = namedtuple('person', ['name', 'age', 'address', 'email', 'phone'])
-#
-# sanch = person(name={'f': 'Sanch', 'l': "Zubchyk", "ln": "Sergeevich"},
-#                age='37',
-#                address='Lida',
-#                email='sanch13@mail.com',
-#                phone=['8091232145', '565789'])
+print(2**32)
+x = "Spam!"
+print(x*8)
