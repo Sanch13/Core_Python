f9eb9b330a4d5884decb45ee2c0b53cfd3b65c55
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-11-09 20:08:33 +0300

    lutz

--- Mark_Lutz/script1.py
+++ Mark_Lutz/script1.py
@@ -1,6 +1,26 @@
 import sys
 
 print(sys.version)
-print(2**32)
+print(2 ** 32)
 x = "Spam!"
-print(x*8)
+print(x * 8)
+
+# while True:
+#     reply = input('Enter text: ')
+#     if reply == 'stop': break
+#     elif not reply.isdigit():
+#         print('Bad!'*8)
+#     else:
+#         print(int(reply)**2)
+# print('Bye')
+
+while True:
+    reply = input('Enter text: ')
+    if reply == 'stop': break
+    try:
+        num = int(reply)
+    except:
+        print('Bad! ' * 8)
+    else:
+        print(num ** 2)
+print('Bye')
