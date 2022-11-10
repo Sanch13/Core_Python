87785fc442c12047411c47456f55f3a92f411482
a.zubchyk <Sanch13sanch@gmail.com>
2022-11-10 16:26:08 +0300

    mark_lutz

--- Mark_Lutz/script1.py
+++ Mark_Lutz/script1.py
@@ -1,9 +1,9 @@
 import sys
 
-print(sys.version)
-print(2 ** 32)
-x = "Spam!"
-print(x * 8)
+
+def func():
+    x = "Spam!"
+    return x * 8
 
 # while True:
 #     reply = input('Enter text: ')
@@ -14,13 +14,16 @@ print(x * 8)
 #         print(int(reply)**2)
 # print('Bye')
 
-while True:
-    reply = input('Enter text: ')
-    if reply == 'stop': break
-    try:
-        num = int(reply)
-    except:
-        print('Bad! ' * 8)
-    else:
-        print(num ** 2)
-print('Bye')
+# while True:
+#     reply = input('Enter text: ')
+#     if reply == 'stop': break
+#     try:
+#         num = int(reply)
+#     except:
+#         print('Bad! ' * 8)
+#     else:
+#         print(num ** 2)
+# print('Bye')
+
+
+print(func(), file=open(r'D:\Python39\Core_Python\txt.txt', 'a'))
