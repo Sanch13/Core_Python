78e3488e07f7cf3087238ef646d0be3b528318e6
a.zubchyk <Sanch13sanch@gmail.com>
2022-11-14 16:47:02 +0300

    Lutz

---
+++ Mark_Lutz/docstring.py
@@ -0,0 +1,17 @@
+"""Module documentation"""
+
+spam = 40
+
+
+def square(x):
+    """function documentation"""
+    return x**2
+
+
+class Employee:
+    """class documentation"""
+    pass
+
+
+print(square(4))
+print(square.__doc__)
