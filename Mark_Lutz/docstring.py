463aa237a16813abdf1ca106e712cde28fb1bbdc
a.zubchyk <Sanch13sanch@gmail.com>
2022-11-15 16:53:58 +0300

    Lutz

--- Mark_Lutz/docstring.py
+++ Mark_Lutz/docstring.py
@@ -1,5 +1,6 @@
 """Module documentation"""
 
+
 # spam = 40
 #
 #
@@ -20,15 +21,17 @@
 #     print('hello %d\n\a' % i)
 
 
-l = [1, 2, 4, 8, 16, 32, 64]
-x = 5
-for i in l:
-    if 2**x == i:
-        print(f'at index', l.index(i))
-        break
+# l = [1, 2, 4, 8, 16, 32, 64]
+# x = 5
+# for i in l:
+#     if 2**x == i:
+#         print(f'at index', l.index(i))
+#         break
+#
+# if 2**x in l:
+#     print(f'at index', l.index(2**x))
+#
+# print(f'at index', *[l.index(i) for i in l if i == 2**x])
 
-if 2**x in l:
-    print(f'at index', l.index(2**x))
 
-print(f'at index', *[l.index(i) for i in l if i == 2**x])
 
