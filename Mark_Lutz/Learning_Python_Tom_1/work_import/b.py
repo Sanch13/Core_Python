d5f36343820f6a6ed89c0c6bca328f6b9a84675a
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-11-27 18:06:11 +0300

    rename

---
+++ Mark_Lutz/Learning_Python_Tom_1/work_import/b.py
@@ -0,0 +1,4 @@
+from Mark_Lutz.Learning_Python_Tom_1.work_import.c import x
+
+def spam(text):
+    return f"{text} spam"
--- Mark_Lutz/Tom_1/work_import/b.py
+++
@@ -1,4 +0,0 @@
-from Mark_Lutz.Tom_1.work_import.c import x
-
-def spam(text):
-    return f"{text} spam"
