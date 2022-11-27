16742b5cf62dbbda4b1fcd59ccd51dc133bf2a1c
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-11-27 15:25:45 +0300

    example_import

---
+++ Mark_Lutz/Tom_1/work_import/a.py
@@ -0,0 +1,19 @@
+# import Mark_Lutz.Tom_1.work_import.b              # 1 way
+# import Mark_Lutz.Tom_1.work_import.b as b         # 2 way
+from Mark_Lutz.Tom_1.work_import.b import spam, x      # 3 way
+
+# print(Mark_Lutz.Tom_1.work_import.b.spam('gumby'))    # 1 way
+# print(b.spam('Gumby'), end='')                        # 2 way
+print(spam('Gumby'))                            # 3 way
+
+
+def set_x(x: int):
+    return x * 10
+
+
+print()
+new_x = set_x(x)
+print(x)
+print()
+print(new_x)
+
